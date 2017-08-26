cff00a46fded18d16c8447cac93bf854ace65de6
Alvin Qi <peng.edy@gmail.com>
2017-08-27 04:44:15 +0800

    Add assets * some channel js to make chatting room work * bootstrap to look beauty * fix some issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
+//= require bootstrap-sprockets
+//= require chatrooms
 //= require_tree .
 //= require_tree ./channels
