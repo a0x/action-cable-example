bad79ddb102daa34255d0957abebb5692e442ff8
Alvin Qi <peng.edy@gmail.com>
2017-08-27 03:22:43 +0800

    Add user login * added user sessions controller * added user login, logout * added related pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   get  'signup', to: "registrations#new"
   post 'signup', to: "registrations#create"
+
+  get    '/login', to: "sessions#new"
+  post   '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
 end
